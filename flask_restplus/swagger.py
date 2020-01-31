a8aecbb8caccff3c25c0781df7559c710316e4b8
Lukas Karlsson <karlsson@broadinstitute.org>
2020-01-31 22:54:58 +0000

    Heh.

--- flask_restplus/swagger.py
+++ flask_restplus/swagger.py
@@ -236,7 +236,7 @@ class Swagger(object):
         if not self.api.extensions or not isinstance(self.api.extensions, dict):
             return specs
         for k, v in self.api.extensions.items():
-            if not k.startswith('-x', k):
+            if not k.startswith('x-'):
                 k = 'x-{}'.format(k)
             specs[k] = v
         return specs
